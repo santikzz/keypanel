e551f626ef100863661813cb4ff7227fa491a54e
santi <monsterspeed02@gmail.com>
2025-03-08 21:29:35 -0300

    webhook logging

--- app/Http/Controllers/PatreonWebhookController.php
+++ app/Http/Controllers/PatreonWebhookController.php
@@ -21,30 +21,30 @@ class PatreonWebhookController extends Controller
             // verify the patreon signature
             $signature = $request->header('X-Patreon-Signature');
 
-            Log::info('Patreon webhook received', ['signature' => $signature]);
+            Log::channel('stack')->info('Patreon webhook received', ['signature' => $signature]);
 
             if (!$this->verifyWebhookSignature($request->getContent(), $signature)) {
                 return response()->json(['error' => 'Invalid signature'], 403);
             }
 
-            Log::info('Patreon webhook verified');
+            Log::channel('stack')->info('Patreon webhook verified');
 
             $payload = $request->all();
             $event = $payload['data']['type'];
 
-            Log::info('Patreon webhook event', ['event' => $event]);
+            Log::channel('stack')->info('Patreon webhook event', ['event' => $event]);
 
             $pledgeAmountCents = $payload['data']['attributes']['amount_cents'] ?? 0;
             $patronId = $payload['data']['relationships']['patron']['data']['id'];
 
-            Log::info('Cents pledged', ['cents' => $pledgeAmountCents]);
-            Log::info('Patreon webhook patron', ['patronId' => $patronId]);
+            Log::channel('stack')->info('Cents pledged', ['cents' => $pledgeAmountCents]);
+            Log::channel('stack')->info('Patreon webhook patron', ['patronId' => $patronId]);
 
             $response = Http::withHeaders(['Authorization' => 'Bearer ' . env('PATREON_API_ACCESS_TOKEN')])
                 ->get("https://www.patreon.com/api/oauth2/v2/user/$patronId", ['fields[user]' => 'email'])->json();
             $patronEmail = $response['data']['attributes']['email'] ?? null;
 
-            Log::info('Patreon webhook patron email', ['email' => $patronEmail]);
+            Log::channel('stack')->info('Patreon webhook patron email', ['email' => $patronEmail]);
 
             if ($patronEmail) {
                 // find the user by email
