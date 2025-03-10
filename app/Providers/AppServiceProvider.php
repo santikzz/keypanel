c79d0fb44189a61d521e4bc6c24179e57850b409
santi <monsterspeed02@gmail.com>
2025-03-10 17:53:11 -0300

    patreon test 2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,5 +27,12 @@ class AppServiceProvider extends ServiceProvider
         Event::listen(function (\SocialiteProviders\Manager\SocialiteWasCalled $event) {
             $event->extendSocialite('patreon', \SocialiteProviders\Patreon\Provider::class);
         });
+
+        // for local https ssl with nginx proxy manager and duckdns, maybe comment on real deploy ?
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+            $this->app['request']->server->set('HTTPS', 'on');
+        }
+
     }
 }
